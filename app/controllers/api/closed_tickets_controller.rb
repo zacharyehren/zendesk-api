8b6bb9d83b4feba02923f7faf5059d1d280d2754
Zachary Ehren <zachehren@gmail.com>
2017-12-22 16:00:02 -0500

    update closed ticket index method, add serialize ticket method

--- app/controllers/api/closed_tickets_controller.rb
+++ app/controllers/api/closed_tickets_controller.rb
@@ -5,7 +5,42 @@ class Api::ClosedTicketsController < ApplicationController
     now = Date.today
     two_weeks_ago = (now - 14)
     sfa_closed_tickets = ZEN_CLIENT.search(:query => "tags:csm tags:sfa_other status:solved updated>" + two_weeks_ago.to_s)
-    puts sfa_closed_tickets
+    ticket_array, submitter_ids = serialize_csm_data(sfa_closed_tickets)
+    users = ZEN_CLIENT.users.show_many(:ids => submitter_ids)
+    submitter_user_data = {}
+    users.all do |user|
+      submitter_user_data[user.id] = user.name
+    end
+    render json: {ticket: ticket_array, users: submitter_user_data}
   end
 
+  private
+    def serialize_csm_data(sfa_data)
+      submitter_ids = []
+      ticket_array = []
+      sfa_data.all do | resource |
+        ticket_array << {
+          id: resource.id,
+          subject: resource.subject,
+          submitter: resource.submitter_id,
+          created_at: resource.created_at,
+          updated_at: resource.updated_at,
+          status: resource.status
+        }
+          submitter_ids << resource.submitter_id
+    end
+    [ticket_array, submitter_ids]
+  end
+
+  # def serialize_comment(comment)
+  #   comment_array = []
+  #   comment.each do | resource |
+  #     sender = resource.author
+  #     comment_array << {
+  #       body: resource.body,
+  #       sender: sender.name }
+  #   end
+  #     comment_array
+  #   end
+  # end
 end
