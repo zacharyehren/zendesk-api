475821be06bf3e8001111a04a278560e0c84ef45
Zachary Ehren <zachehren@gmail.com>
2018-01-29 21:06:54 -0800

    add username data to serialized ticket hash

--- app/controllers/api/closed_tickets_controller.rb
+++ app/controllers/api/closed_tickets_controller.rb
@@ -5,30 +5,8 @@ class Api::ClosedTicketsController < ApplicationController
     now = Date.today
     two_weeks_ago = (now - 14)
     sfa_closed_tickets = ZEN_CLIENT.search(:query => "tags:csm tags:sfa_other status:solved status:closed order_by:updated_at updated>" + two_weeks_ago.to_s)
-    ticket_array, submitter_ids = serialize_csm_data(sfa_closed_tickets)
-    users = ZEN_CLIENT.users.show_many(:ids => submitter_ids)
-    submitter_user_data = {}
-    users.all do |user|
-      submitter_user_data[user.id] = user.name
-    end
-    render json: {ticket: ticket_array, users: submitter_user_data}
+    ticket_array = serialize_ticket_data(sfa_closed_tickets)
+    render json: {ticket: ticket_array }
   end
 
-  private
-    def serialize_csm_data(sfa_data)
-      submitter_ids = []
-      ticket_array = []
-      sfa_data.all do | resource |
-        ticket_array << {
-          id: resource.id,
-          subject: resource.subject,
-          submitter: resource.submitter_id,
-          created_at: resource.created_at,
-          updated_at: resource.updated_at,
-          status: resource.status
-        }
-          submitter_ids << resource.submitter_id
-    end
-    [ticket_array, submitter_ids]
-  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::API
+
+  def serialize_ticket_data(sfa_data)
+    submitter_ids = []
+    ticket_array = []
+    sfa_data.all do | resource |
+      ticket_array << {
+        id: resource.id,
+        subject: resource.subject,
+        submitter: resource.submitter_id,
+        created_at: resource.created_at,
+        updated_at: resource.updated_at,
+        status: resource.status
+      }
+        submitter_ids << resource.submitter_id
+    end
+    users = ZEN_CLIENT.users.show_many(:ids => submitter_ids)
+    submitter_user_data = {}
+    users.all do |user|
+      submitter_user_data[user.id] = user.name
+    end
+    ticket_array.each do | ticket |
+      submitter_id = ticket[:submitter]
+      ticket[:username] = submitter_user_data[submitter_id]
+    end
+    ticket_array
+  end
 end
