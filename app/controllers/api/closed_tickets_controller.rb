f4895a8e6f95fa1a8a19753828b293d9f093d619
Zachary Ehren <zachehren@gmail.com>
2018-02-07 15:04:33 -0800

    remove hardcoded submitter query

--- app/controllers/api/closed_tickets_controller.rb
+++ app/controllers/api/closed_tickets_controller.rb
@@ -4,8 +4,8 @@ class Api::ClosedTicketsController < ApplicationController
   def index
     now = Date.today
     two_weeks_ago = (now - 14)
-    sfa_closed_tickets = ZEN_CLIENT.search(:query => "tags:csm tags:sfa_other status:solved status:closed order_by:updated_at updated>" + two_weeks_ago.to_s)
-    ticket_array = serialize_ticket_data(sfa_closed_tickets)
+    sfa_closed_tickets = ZEN_CLIENT.search(:query => "tags:csm tags:sfa_other status:solved status:closed order_by:updated_at updated>#{two_weeks_ago.to_s}")
+    ticket_array, incident_ticket_array = serialize_ticket_data(sfa_closed_tickets)
     render json: {ticket: ticket_array, incidents: incident_ticket_array }
   end
 
--- app/controllers/api/my_tickets_controller.rb
+++ app/controllers/api/my_tickets_controller.rb
@@ -1,11 +1,10 @@
 class Api::MyTicketsController < ApplicationController
 
   def index
-    submitter = params[:user_name]
-    sfa_tickets = ZEN_CLIENT.search(:query => "status<solved order_by:created_at submitter:zehren@sharethrough.com")
+    submitter = params[:user_email]
+    sfa_tickets = ZEN_CLIENT.search(:query => "status<solved order_by:created_at submitter:#{submitter}")
     ticket_array, incident_ticket_array = serialize_ticket_data(sfa_tickets)
     render json: {ticket: ticket_array, incidents: incident_ticket_array}
   end
-
 
 end
