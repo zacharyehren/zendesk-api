c028586bc82f9e847f2634096f04306644e5023c
Zachary Ehren <zachehren@gmail.com>
2018-02-06 18:50:28 -0800

    refactor serialize_ticket_data method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,28 +3,51 @@ class ApplicationController < ActionController::API
   def serialize_ticket_data(sfa_data)
     submitter_ids = []
     ticket_array = []
+    incident_ticket_array = []
     sfa_data.all do | resource |
-      ticket_array << {
-        id: resource.id,
-        problem_id: resource.problem_id,
-        type: resource.type,
-        subject: resource.subject,
-        submitter: resource.submitter_id,
-        created_at: resource.created_at,
-        updated_at: resource.updated_at,
-        status: resource.status,
-      }
-        submitter_ids << resource.submitter_id
+      if resource.type == "incident"
+        incident_ticket_array << {
+          id: resource.id,
+          problem_id: resource.problem_id,
+          type: resource.type,
+          subject: resource.subject,
+          submitter: resource.submitter_id,
+          created_at: resource.created_at,
+          updated_at: resource.updated_at,
+          status: resource.status,
+          }
+      else
+        ticket_array << {
+          id: resource.id,
+          type: resource.type,
+          subject: resource.subject,
+          submitter: resource.submitter_id,
+          created_at: resource.created_at,
+          updated_at: resource.updated_at,
+          status: resource.status,
+          }
     end
+    submitter_ids << resource.submitter_id
+  end
+
     users = ZEN_CLIENT.users.show_many(:ids => submitter_ids)
+
     submitter_user_data = {}
+
     users.all do |user|
       submitter_user_data[user.id] = user.name
     end
-    ticket_array.each do | ticket |
-      submitter_id = ticket[:submitter]
-      ticket[:username] = submitter_user_data[submitter_id]
+
+    def create_users(ticket_object, submitter_user_data)
+      ticket_object.each do | ticket |
+        submitter_id = ticket[:submitter]
+        ticket[:username] = submitter_user_data[submitter_id]
+      end
     end
-    ticket_array
+
+    create_users(ticket_array, submitter_user_data)
+    create_users(incident_ticket_array, submitter_user_data)
+
+    [ticket_array, incident_ticket_array]
   end
 end
